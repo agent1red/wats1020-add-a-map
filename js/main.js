6539538181ccc647f8cd04b925d240acdf088b64
hudsonk2 <hudsonk2@seattleu.edu>
2016-10-31 02:19:56 -0400

    updated the js file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* Begin by adding your on ready handler here, and then create the
    rest of your functions inside the on ready handler.
-
+   
    (Note that you do not need to manually call Bootstrap functions in
    your Javascript because Bootstrap will automatically recognize your
    HTML structures and invoke the proper JS code accordingly. Be sure
@@ -19,3 +19,26 @@
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+$(document).ready(function() {
+
+  var mymap = L.map('map-container').setView([33.812142, -117.918625], 18);
+  L.tileLayer(' https://api.mapbox.com/styles/v1/agent1red/ciuxhyam800972iphfaydyp1w/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.mapbox-streets-v7',
+    accessToken: 'pk.eyJ1IjoiYWdlbnQxcmVkIiwiYSI6ImNpdXhnZzRoeDA0amYyeW4wcGpoaTBoOHQifQ.b_neNrsCyLsE5xtH4684TQ'
+  }).addTo(mymap);
+
+  var marker = L.marker([33.812142, -117.918625])
+   .addTo(mymap);
+  
+    var marker1 = L.marker([33.812100, -117.918306])
+   .addTo(mymap);
+  
+  var marker2 = L.marker([33.812100, -117.918969])
+    .addTo(mymap)
+    .bindPopup("<b>Disneyland</b><br>Disneyland Park")
+    .openPopup();
+
+ 
+});
